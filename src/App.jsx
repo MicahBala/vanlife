2a2f450b9445ab7cf47efd41331195c1ac062f72
micahbala <micahkulien@.com>
2023-09-09 18:28:34 +0100

    finish van detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
+import VanList from './pages/VanList.jsx'
+import VanDetail from './pages/VanDetail.jsx'
+
+import './server.jsx'
 
 function App() {
   return (
@@ -11,11 +15,14 @@ function App() {
         </Link>
         <nav>
           <Link to='/about'>About</Link>
+          <Link to='/vans'>Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<VanList />} />
+        <Route path='/vans/:id' element={<VanDetail />} />
       </Routes>
     </>
   )
