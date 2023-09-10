7b88970f95dd0c1133ec79f90b150958a6fc9316
micahbala <micahkulien@.com>
2023-09-10 16:32:34 +0100

    add layout route

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
-import VanList from './pages/VanList.jsx'
-import VanDetail from './pages/VanDetail.jsx'
-
+import VanList from './pages/vans/VanList.jsx'
+import VanDetail from './pages/vans/VanDetail.jsx'
+import Layout from './components/Layout.jsx'
 import './server.jsx'
+import Dashboard from './pages/host/Dashboard.jsx'
+import Income from './pages/host/Income.jsx'
+import Reviews from './pages/host/Reviews.jsx'
 
 function App() {
   return (
     <>
-      <header className='container'>
-        <Link className='site-logo' to='/'>
-          #VANLIFE
-        </Link>
-        <nav>
-          <Link to='/about'>About</Link>
-          <Link to='/vans'>Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/vans' element={<VanList />} />
-        <Route path='/vans/:id' element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/vans' element={<VanList />} />
+          <Route path='/vans/:id' element={<VanDetail />} />
+          <Route path='/host' element={<Dashboard />} />
+          <Route path='/host/income' element={<Income />} />
+          <Route path='/host/reviews' element={<Reviews />} />
+        </Route>
       </Routes>
     </>
   )
---
+++ src/pages/host/Dashboard.jsx
@@ -0,0 +1,5 @@
+function Dashboars() {
+  return <div>Dashboars</div>
+}
+
+export default Dashboars
